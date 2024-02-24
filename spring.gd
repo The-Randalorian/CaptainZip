49db550dbed338d11ea6822c6b09715698e16dc7
The Randalorian <dwjgame11@gmail.com>
2024-02-24 10:13:23 -0500

    Add Background

--- spring.gd
+++ spring.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 
 @export var strength = 1000
+var allow_spring = true
 
 
 func _on_body_entered(body):
-	if not $AnimatedSprite2D.is_playing():
-		body.velocity.y -= strength
+	if allow_spring and not $AnimatedSprite2D.is_playing():
+		body.velocity.y = -strength
+		allow_spring = false
 		$AnimatedSprite2D.play("default")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	allow_spring = true
