2f680745bb266de0dd51aae6f5f2f634d739d0be
Eric Weakley <draqonhearted@gmail.com>
2024-02-25 18:41:16 -0500

    B button remap fix

--- Misc_Scripts/Hint Scripts/hint.gd
+++ Misc_Scripts/Hint Scripts/hint.gd
@@ -4,10 +4,13 @@ class_name Hint
 
 func _event_to_string(event,default = ""):
 	if event is InputEventKey:
+		self.set_process_input(false)
 		return event.as_text_physical_keycode()
 	elif event is InputEventJoypadButton:
+		self.set_process_input(false)
 		return "Button " + str(event.button_index)
 	elif event is InputEventJoypadMotion:
+		self.set_process_input(false)
 		return "Axis " + str(event.axis)
 	else:
 		return default
