08502e546057b4f4330a7979b99523d4b4c2b682
The Randalorian <dwjgame11@gmail.com>
2024-02-25 12:58:00 -0500

    menu touch up

--- main_menu.gd
+++ main_menu.gd
@@ -12,3 +12,7 @@ func _process(delta):
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/level 1.tscn") # Replace with function body.
+
+
+func _on_quit_button_button_down():
+	get_tree().quit()
