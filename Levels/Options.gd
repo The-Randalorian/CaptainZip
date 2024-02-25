acab6258c9ad742cad49ca8eb7b09cb588a3be12
Eric Weakley <draqonhearted@gmail.com>
2024-02-25 15:37:16 -0500

    Controller input support

--- Levels/Options.gd
+++ Levels/Options.gd
@@ -14,17 +14,27 @@ func _on_buttonChange_press(option):
 	EditStr = option
 
 func _input(event):
-	if event is InputEventKey and EditStr != null:
-		if event.pressed:
-			var temp = EditStr
-			if temp != "":
-				EditStr = ""
-				InputMap.action_erase_events(temp)
-				InputMap.action_add_event(temp, event)
-				var labelEdit = get_node("%" + temp + "_label")
-				if labelEdit != null:
-					labelEdit.text = event.as_text_key_label()
+	if(EditStr != null):
+		if (event is InputEventKey or event is InputEventJoypadButton) and event.pressed:
+				var remapText = ""
+				if event is InputEventKey:
+					remapText = event.as_text_key_label()
+				elif event is InputEventJoypadButton:
+					remapText = "Button " + str(event.button_index)
+				_changeInputText(event,remapText)
+		if event is InputEventJoypadMotion and (event.axis_value > 0.8 or event.axis_value < -0.8):
+				_changeInputText(event,"axis " + str(event.get_axis()))
 			
+
+func _changeInputText(event,labelText):
+	var temp = EditStr
+	if temp != "":
+		EditStr = null
+		InputMap.action_erase_events(temp)
+		InputMap.action_add_event(temp, event)
+		var labelEdit = get_node("%" + temp + "_label")
+		if labelEdit != null:
+			labelEdit.text = labelText
 
 func _on_jump_pressed():
 	_on_buttonChange_press("move_jump")
@@ -36,3 +46,7 @@ func _on_move_right_pressed():
 
 func _on_move_left_pressed():
 	_on_buttonChange_press("move_left")
+
+
+func _on_detach_pressed():
+	_on_buttonChange_press("move_detach")
